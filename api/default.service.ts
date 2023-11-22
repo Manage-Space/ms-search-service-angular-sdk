54733697f1af43877bae1bde191e5f8a9ecf8af6
managespace <89160931+managespace@users.noreply.github.com>
2023-11-22 19:30:02 +0000

    Build 041d6e7a-2ca7-40c2-9d6e-4402345f4700 on Thu Jun 12 07:58:45 UTC 55862 (Commit: W6vqCwMd0vQaR9Kj8f48bjHOU63yFYaJ)

--- api/default.service.ts
+++ api/default.service.ts
@@ -42,7 +42,7 @@ import { Configuration }                                     from '../configurat
 })
 export class DefaultService {
 
-    protected basePath = 'http://localhost';
+    protected basePath = 'https://api.dev.manage-space.com';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
     public encoder: HttpParameterCodec;
